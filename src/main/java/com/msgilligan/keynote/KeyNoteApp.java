310dbfd556c832ce0fc8df5a8d216aab2d8ccdfb
Sean Gilligan <sean@msgilligan.com>
2021-05-30 00:39:27 -0700

    Use Sample SupernautFX Gradle project
    Packages were renamed, etc.

---
+++ src/main/java/com/msgilligan/keynote/KeyNoteApp.java
@@ -0,0 +1,45 @@
+package com.msgilligan.keynote;
+
+import app.supernaut.fx.FxForegroundApp;
+import app.supernaut.fx.FxLauncher;
+import app.supernaut.services.BrowserService;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import jakarta.inject.Singleton;
+import java.net.URI;
+
+@Singleton
+public class KeyNoteApp implements FxForegroundApp.FxApplicationCompat  {
+    private static final URI githubRepoUri = URI.create("https://github.com/SupernautApp/SupernautFX");
+    private final BrowserService browserService;
+
+    public KeyNoteApp(BrowserService browserService) {
+        this.browserService = browserService;
+    }
+
+    @Override
+    public void start(Stage stage) {
+        stage.setScene(buildScene());
+        stage.show();
+    }
+
+    private Scene buildScene() {
+        var javaVersion = System.getProperty("java.version");
+        var javafxVersion = System.getProperty("javafx.version");
+        var label       = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var hyperlink   = new Hyperlink("Powered by Supernaut.FX");
+        var vbox        = new VBox(label, hyperlink);
+        vbox.setAlignment(Pos.CENTER);
+        hyperlink.setOnAction(e -> browserService.showDocument(githubRepoUri));
+        return new Scene(vbox, 350, 100);
+    }
+
+    public static void main(String[] args) {
+        FxLauncher.byName("micronaut").launch(args, KeyNoteApp.class);
+    }
+}
